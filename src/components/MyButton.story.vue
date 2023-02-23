c802c7b62aa217f13b53fe106d5aadd8a7202c6c
AnaelBonnafous <71386173+AnaelBonnafous@users.noreply.github.com>
2023-02-23 15:35:00 +0100

    feat(ui): install and setup Histoire

---
+++ src/components/MyButton.story.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import MyButton from "./MyButton.vue";
+</script>
+
+<template>
+  <Story>
+    <Variant title="Default">
+      <MyButton>Default</MyButton>
+    </Variant>
+    <Variant title="Success">
+      <MyButton color="#4caf50">Success</MyButton>
+    </Variant>
+    <Variant title="Error">
+      <MyButton color="#f44336">Error</MyButton>
+    </Variant>
+    <Variant title="Info">
+      <MyButton color="#2196f3">Info</MyButton>
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title?: string;
+    color?: string;
+  }>(),
+  { title: "", color: "#000000" }
+);
+</script>
+
+<template>
+  <button :title="title || undefined">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+button {
+  color: #fff;
+  background-color: v-bind("color");
+  padding: 10px;
+  border-radius: 10px;
+  border: none;
+}
+
+button:hover {
+  cursor: pointer;
+  filter: opacity(0.8);
+  outline: 2px solid v-bind("color");
+  outline-offset: 2px;
+}
+</style>
