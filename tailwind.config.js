d30e5daba904ce9b720ba559d664fddbf61584df
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-03-09 15:36:32 +0100

    feat(css): install and configure Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/components/**/*.{js,vue,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
